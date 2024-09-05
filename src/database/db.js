a1ba8968c0a373ba7f8eed2b54699d5b8b40a0f3
Domingos Canhanga <canhanga96@gmail.com>
2024-09-05 22:43:36 +0100

    feat: add databasejson

---
+++ src/database/db.js
@@ -0,0 +1,58 @@
+import fs from 'node:fs/promises'
+const databasePath = new URL('./db.json', import.meta.url)
+export class BataBase {
+  #database = {}
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
+  construtor() {
+    fs.readfile(databasePath, 'utf-8').then(data => {
+      this.#database = JSON.parse(data)
+    }).catch(() => {
+      this.#persist()
+    })
+  }
+  select(table, filter) {
+    let data = this.#database[table] ?? []
+    if (!filter) {
+      return data
+    }
+    const { id, ...filterWithoutId } = filter
+    if (id) {
+      return data.find(item => item.id === id)
+    }
+    if (filterWithoutId.name || filterWithoutId.email) {
+      data = data.filter(row => {
+        return Object.entries(filterWithoutId).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
+    return data
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
+    }
+    this.#persist()
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
+    }
+  }
+}
